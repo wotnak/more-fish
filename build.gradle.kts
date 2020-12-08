2498603868d892350ff5c506a6c958726487222f
wotnak <adamwotnak@gmail.com>
2020-12-08 15:12:09 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,40 +17,48 @@ val mainClass = "$mainPackage.MoreFish"
 
 repositories {
     jcenter()
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven("http://nexus.hc.to/content/repositories/pub_releases")
-    maven("http://repo.citizensnpcs.co/")
-    maven("http://repo.extendedclip.com/content/repositories/placeholderapi/")
-    maven("http://repo.dmulloy2.net/nexus/repository/public/")
-    maven("http://maven.sk89q.com/repo/")
+    maven("https://repo.citizensnpcs.co/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.dmulloy2.net/nexus/repository/public/")
+    maven("https://maven.enginehub.org/repo/")
     maven("https://repo.aikar.co/content/groups/aikar/")
-    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+    maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://jitpack.io")
+    maven {
+        setUrl("https://papermc.io/ci/view/%20%20Plugins/job/mcMMO/com.gmail.nossr50.mcMMO\$mcMMO/1107/artifact/")
+        artifactUrls("https://papermc.io/ci/view/%20%20Plugins/job/mcMMO/com.gmail.nossr50.mcMMO\$mcMMO/1107/artifact/com.gmail.nossr50.mcMMO/")
+        metadataSources {
+            mavenPom()
+            artifact()
+        }
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.0.0")
 
-    implementation("co.aikar:acf-core:0.5.0-SNAPSHOT")
     implementation("co.aikar:acf-paper:0.5.0-SNAPSHOT")
     implementation("com.github.elsiff:egui:1.0.2-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib-API:4.2.1")
-    compileOnly("com.sk89q:worldguard:6.1")
-    compileOnly("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
-    compileOnly("net.milkbowl.vault:VaultAPI:1.6")
-    compileOnly("net.citizensnpcs:citizensapi:2.0.18-SNAPSHOT")
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib-API:4.4.0")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.4")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("net.citizensnpcs:citizensapi:2.0.27-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.10.9")
-    compileOnly("com.github.mcMMO-Dev:mcMMO:fa1bbd2")
+    //compileOnly("com.github.mcMMO-Dev:mcMMO:dacd846fe7")
+    compileOnly(".:mcMMO:2.1.159")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-parameters")
+    options.isFork = true
+    options.forkOptions.executable = "javac"
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.javaParameters = true
 }
 
 tasks.withType<ShadowJar> {
